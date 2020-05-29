38fba9f1fd980d6f9ac4c0cac4c842459640bb00
José Luiz Junior <junior.jr.jl@gmail.com>
2020-05-29 17:17:54 -0300

    criação de TODO para desafio futuro

--- app/jobs/campaign_raffle_job.rb
+++ app/jobs/campaign_raffle_job.rb
@@ -3,5 +3,14 @@ class CampaignRaffleJob < ApplicationJob
 
   def perform(campaign)
     results = RaffleService.new(campaign).call
+
+    campaign.members.each {|m| m.set_pixel}
+    result.each do |r|
+      CampaignMailer.raffle(campaign, r.first, r.last).delivery_now
+    end
+    campaign.update(status: :finished)
+
+    #TODO fazer envio de email notificando sobre problemas    
+
   end
 end
